297b6ce8ab426f92876c08b6e972dff0e98fcdd3
heroboec <zealot351@gmail.com>
2025-09-04 18:23:01 +0300

    Fix some style in migrations files.

--- src/migrations/versions/380fae49184d_rooms_model_added.py
+++ src/migrations/versions/380fae49184d_rooms_model_added.py
@@ -20,15 +20,16 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.create_table('rooms',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('hotel_id', sa.Integer(), nullable=False),
-    sa.Column('title', sa.String(), nullable=False),
-    sa.Column('description', sa.String(), nullable=True),
-    sa.Column('price', sa.Integer(), nullable=False),
-    sa.Column('quantity', sa.Integer(), nullable=False),
-    sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
-    sa.PrimaryKeyConstraint('id')
+    op.create_table(
+        'rooms',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('hotel_id', sa.Integer(), nullable=False),
+        sa.Column('title', sa.String(), nullable=False),
+        sa.Column('description', sa.String(), nullable=True),
+        sa.Column('price', sa.Integer(), nullable=False),
+        sa.Column('quantity', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
+        sa.PrimaryKeyConstraint('id')
     )
 
 
