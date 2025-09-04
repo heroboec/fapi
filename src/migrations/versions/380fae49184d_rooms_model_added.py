a5aeeffad9608355d8f0650cae9c9ce7ec31d793
heroboec <zealot351@gmail.com>
2025-09-04 18:19:19 +0300

    Model rooms added.

---
+++ src/migrations/versions/380fae49184d_rooms_model_added.py
@@ -0,0 +1,37 @@
+"""rooms model added
+
+Revision ID: 380fae49184d
+Revises: 460f90a083b8
+Create Date: 2025-09-04 18:18:09.544949
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '380fae49184d'
+down_revision: Union[str, Sequence[str], None] = '460f90a083b8'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table('rooms',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('hotel_id', sa.Integer(), nullable=False),
+    sa.Column('title', sa.String(), nullable=False),
+    sa.Column('description', sa.String(), nullable=True),
+    sa.Column('price', sa.Integer(), nullable=False),
+    sa.Column('quantity', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['hotel_id'], ['hotels.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('rooms')
