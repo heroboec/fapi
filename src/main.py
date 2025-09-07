4505ca7bb7c6a3ae2f9221f6aadc49456e1dde62
heroboec <zealot351@gmail.com>
2025-09-07 18:09:17 +0300

    Registration api added.

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ import uvicorn
 import sys
 sys.path.append(str(Path(__file__).parent.parent))
 from src.api.hotels import router as hotel_router
+from src.api.auth import router as user_router
 
 app = FastAPI()
+app.include_router(user_router)
 app.include_router(hotel_router)
 
 if __name__ == "__main__":
